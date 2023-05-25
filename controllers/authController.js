3fb6d45d1e5c765597f047a25351ebbd639f73ae
Muhammad Bilal <mbilal.dev4@gmail.com>
2023-05-25 14:06:48 +0500

    Update_SignUp_Method

--- controllers/authController.js
+++ controllers/authController.js
@@ -53,14 +53,24 @@ const createSendToken = (user, statusCode, res, message) => {
   });
 };
 exports.signup = catchAsync(async (req, res) => {
-  const user = await User.create({
-    email: req.body.email,
-    password: req.body.password,
-    role: req.body.role,
-    passwordConfirm: req.body.passwordConfirm,
-  })
+  const { email, password, role, passwordConfirm, categories, subcategories, companyType } = req.body;
+  const user = new User({
+    email,
+    password,
+    role,
+    passwordConfirm,
+    companyType
+  });
+
+  if (categories) {
+    user.Categories = categories;
+  }
+
+  if (subcategories) {
+    user.subCategories = subcategories;
+  }
   const ResetOtp = await user.createotp()
-  await user.save({ validateBeforeSave: false });
+  await user.save();
   const message = `Please Vierify your Account with This OTP ${ResetOtp}.`
   try {
     await sendEmail({
@@ -103,9 +113,12 @@ exports.login = catchAsync(async (req, res, next) => {
   if (!(email, password)) {
     return next(new AppError('please provide email and password !', 400))
   }
-  const user = await User.findOne({ email }).select('+password')
+  const user = await User.findOne({ email }).select('+password +isTrue')
   if (!user || !await user.correctPassword(password, user.password)) {
-    return next(new AppError('Incorrect email or password!', 401))
+    return next(new AppError('Incorrect email or password! or please verify again', 401))
+  }
+  if (!user.isTrue ) {
+    return next(new AppError('You are not verify Please verify again!', 401))
   }
   createSendToken(user, 200, res)
 }
