63f0d9f35fc8a2d3a6a8a63fecf973e1845e5a3f
bilal060 <ahmad_4al@hotmail.com>
2023-05-27 09:56:05 +0500

    Model changes

--- models/spaceModel.js
+++ models/spaceModel.js
@@ -4,17 +4,18 @@ const Schema = mongoose.Schema;
 
 const spaceSchema = new Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    category: {
-        type: String,
-        enum: ['Truck', 'Car', 'Warehouse', 'Storage'],
-        require: true
-    },
+    categoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
+    subCategoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
     area: { type: String },
     contact: { type: String },
     security: { type: String },
     cameras: { type: String },
+    ownerSite: { type: Boolean, required: true },
+    paidStaff: { type: Boolean, required: true },
+    paidSecurity: { type: Boolean, required: true },
+    climateControl: { type: Boolean, required: true },
     capacity: { type: String },
-    fuel: { type: String },
+    fuel: { type: Boolean, required: true },
     rate_hour: { type: Number },
     rate_day: { type: Number },
     rate_week: { type: Number },
