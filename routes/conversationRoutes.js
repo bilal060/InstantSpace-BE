c6c50e744f29eb4c09d25d466a171485d756ae4b
bilal060 <ahmad_4al@hotmail.com>
2023-05-29 12:12:13 +0500

    Changes

--- routes/conversationRoutes.js
+++ routes/conversationRoutes.js
@@ -2,16 +2,17 @@ const express = require('express');
 const { check } = require('express-validator');
 
 const conversationController = require('../controllers/conversationController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-router.get('/', conversationController.all_conversations);
+router.get('/', authController.protect, conversationController.all_conversations);
 
-router.get('/:userId', conversationController.get_user_conversations);
+router.get('/:userId', authController.protect, conversationController.get_user_conversations);
 
-router.post('/', conversationController.new_conversation);
+router.post('/', authController.protect, conversationController.new_conversation);
 
-router.post('/add-user/:convId', [
+router.post('/add-user/:convId', authController.protect, [
     check('userId').not().isEmpty(),
 ], conversationController.addUserToConversation);
 
