0c4c25bc5dcc3e79e5a4bfa93949784a1639fe8f
bilal060 <ahmad_4al@hotmail.com>
2023-05-31 11:44:41 +0500

    Changes

--- models/messageModel.js
+++ models/messageModel.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const messageSchema = new Schema({ 
-    conversationId: [{ type: mongoose.Schema.ObjectId, ref: 'Conversation' }],
+const messageSchema = new Schema({
+    conversationId: { type: mongoose.Schema.ObjectId, ref: 'Conversation' },
     sender: { type: String },
     message: { type: String }
 },
