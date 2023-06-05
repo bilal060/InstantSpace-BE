8ff08fa988b3a0235e58e43aa2c94457d3ddde10
bilal060 <ahmad_4al@hotmail.com>
2023-06-05 09:47:16 +0500

    Changes

--- Helper/photoUpload.js
+++ Helper/photoUpload.js
@@ -4,18 +4,18 @@ const driverProfileUpload = require('../middlewares/driverProfile-upload');
 const multer = require('multer');
 exports.photoUpload = (req, res, next) => {
   console.log(req.user.role);
-  if (req.user.role === 'Business Owner') {
-    fileUpload.single('c_docs')(req, res, err => {
-      if (
-        err instanceof multer.MulterError &&
-        err.code === 'LIMIT_UNEXPECTED_FILE'
-      ) {
-        err = null;
-      }
-      next(err);
-    });
-  }
-  else if (req.user.role === 'Storage Owner') {
+  // if (req.user.role === 'Business Owner') {
+  //   fileUpload.single('c_docs')(req, res, err => {
+  //     if (
+  //       err instanceof multer.MulterError &&
+  //       err.code === 'LIMIT_UNEXPECTED_FILE'
+  //     ) {
+  //       err = null;
+  //     }
+  //     next(err);
+  //   });
+  // }
+  if (req.user.role === 'Storage Owner') {
     fileUpload.single('c_docs')(req, res, err => {
       if (
         err instanceof multer.MulterError &&
